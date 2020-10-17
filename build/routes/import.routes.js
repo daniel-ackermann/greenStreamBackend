7fbce575c93d1906f75750af2e2732e807f65ca2
dede53 <hallomarcel.hm@gmail.com>
2020-10-17 22:59:13 +0200

    remove build

--- build/routes/import.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const middleware_1 = require("../middleware");
-const import_controller_1 = require("../controllers/import.controller");
-const router = express_1.Router();
-router.route('/')
-    .post(middleware_1.authenticate, import_controller_1.importJSON);
-exports.default = router;
