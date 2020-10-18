564ea3f634dba978e7e20aabc1c171d3daab41e8
dede53 <hallomarcel.hm@gmail.com>
2020-10-18 13:05:32 +0200

    fix header send after send

--- src/controllers/import.controller.ts
+++ src/controllers/import.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
+import { pipeline } from "nodemailer/lib/xoauth2";
 import { storeItem } from "../controllers/item.controller"
 import { Item } from "../interface/item";
 
-export async function importJSON(req: Request, res: Response): Promise<void> {
+export async function importJSON(req: Request, res: Response): Promise<Response> {
     const data = JSON.parse(req.body.data);
-    data.forEach((item: any) => {
+    for(let index = 0; index < data.length; index++ ){
+        let item = data[index];
         if (item.title == '' || item.description == '') {
-            return;
+            continue;
         }
         if (item.explanation_id == 'N') {
             item.explanation_id = 0;
@@ -22,6 +24,27 @@ export async function importJSON(req: Request, res: Response): Promise<void> {
             explanation_id: item.explanation_id,
             language: ["de"]
         }
-        storeItem(newItem);
-    });
+        await storeItem(newItem);
+    }
+    return res.json(200);
+    // data.forEach((item: any) => {
+    //     if (item.title == '' || item.description == '') {
+    //         return;
+    //     }
+    //     if (item.explanation_id == 'N') {
+    //         item.explanation_id = 0;
+    //     }
+    //     const newItem: Item = {
+    //         title: item.title,
+    //         description: item.description,
+    //         url: item.url || '',
+    //         topic_id: item.topic_id || 0,
+    //         type_id: item.type_id || 0,
+    //         likes: item.likes || 0,
+    //         simple: item.simple || 0,
+    //         explanation_id: item.explanation_id,
+    //         language: ["de"]
+    //     }
+    //     storeItem(newItem);
+    // });
 }
