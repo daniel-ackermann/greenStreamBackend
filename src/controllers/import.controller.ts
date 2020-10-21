b8bb857af8acdf15381d8b61f52005d37b43dbf4
dede53 <hallomarcel.hm@gmail.com>
2020-10-21 02:20:57 +0200

    refactor

--- src/controllers/import.controller.ts
+++ src/controllers/import.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
-import { pipeline } from "nodemailer/lib/xoauth2";
-import { storeItem } from "../controllers/item.controller"
+import { addItem } from "../controllers/item.controller"
 import { Item } from "../interface/item";
 
 export async function importJSON(req: Request, res: Response): Promise<Response> {
@@ -24,27 +23,7 @@ export async function importJSON(req: Request, res: Response): Promise<Response>
             explanation_id: item.explanation_id,
             language: ["de"]
         }
-        await storeItem(newItem);
+        await addItem(newItem);
     }
     return res.json(200);
-    // data.forEach((item: any) => {
-    //     if (item.title == '' || item.description == '') {
-    //         return;
-    //     }
-    //     if (item.explanation_id == 'N') {
-    //         item.explanation_id = 0;
-    //     }
-    //     const newItem: Item = {
-    //         title: item.title,
-    //         description: item.description,
-    //         url: item.url || '',
-    //         topic_id: item.topic_id || 0,
-    //         type_id: item.type_id || 0,
-    //         likes: item.likes || 0,
-    //         simple: item.simple || 0,
-    //         explanation_id: item.explanation_id,
-    //         language: ["de"]
-    //     }
-    //     storeItem(newItem);
-    // });
 }
