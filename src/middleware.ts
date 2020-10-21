629639c3ea9f7122e4377167dbd395f06e69a029
dede53 <hallomarcel.hm@gmail.com>
2020-10-21 02:20:30 +0200

    add token to req

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,25 @@
-import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken"
 import "dotenv/config"
+import { Response, NextFunction, Request } from "express";
 import { cookieToken } from "./interface/cookieToken";
 
-export function authenticate(req: Request, res: any, next: NextFunction): any {
+export function authenticate(req: Request, res: Response, next: NextFunction){
+    
     const authHeader = req.cookies.jwt;
-    if (authHeader && hasValidToken(authHeader)) {
+    req.token = hasValidToken(authHeader);
+    if (authHeader &&  req.token != false ) {
         next();
     } else {
         return res.status(401).send("401");
     }
 }
 
-export function hasValidToken(authHeader: string): boolean{
+export function hasValidToken(authHeader: string): false|cookieToken{
     try {
         const token = jwt.verify(authHeader.trim(), process.env.ACCESS_TOKEN_SECRET as string) as cookieToken;
         // Sollte ich hier prüfen ob der nutzer noch in der db ist? Nee der token läuft schnell genug ab...
         if (token.exp > Math.floor(Date.now() / 1000) && token.role == "admin") {
-            return true;
+            return token;
         } else {
             return false;
         }
