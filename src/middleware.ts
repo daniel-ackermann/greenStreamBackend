71cfcf33d34e99cb6a1218c6a716383d695cbb68
dede53 <hallomarcel.hm@gmail.com>
2020-10-31 10:35:34 +0100

    accept members

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,7 +18,7 @@ export function hasValidToken(authHeader: string): false|cookieToken{
     try {
         const token = jwt.verify(authHeader.trim(), process.env.ACCESS_TOKEN_SECRET as string) as cookieToken;
         // Sollte ich hier prüfen ob der nutzer noch in der db ist? Nee der token läuft schnell genug ab...
-        if (token.exp > Math.floor(Date.now() / 1000) && token.role == "admin") {
+        if (token.exp > Math.floor(Date.now() / 1000) && token.role === "admin" || token.role === "member") {
             return token;
         } else {
             return false;
