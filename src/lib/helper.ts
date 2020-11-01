b4b28b51fe20229628df9a7366ba96aadcea66d2
dede53 <hallomarcel.hm@gmail.com>
2020-11-01 13:44:38 +0100

    add standard languages

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,18 +1,13 @@
 
-export function parseLanguage(lang: string = "de"): string[] {
+export function parseLanguage(lang = "de,en,es"): string[] {
     if (lang.length === 0) {
-        return ["de%"];
+        return ["de"];
     }
 
-    // wildcard for all
-    if (lang === '*') {
-        return ['%'];
-    }
-
-    // Split by , then by ; then returns the country-code with %
-    let pairs = lang.split(',');
-    let result = [];
-    for (let i in pairs) {
+    // Split by , then by ; 
+    const pairs = lang.split(',');
+    const result = [];
+    for (const i in pairs) {
         result.push(pairs[i].split(';')[0].split('-')[0]);
     }
     // remove duplicates
