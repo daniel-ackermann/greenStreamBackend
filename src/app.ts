1f9213b6b43227bac9b81ebef6b106fe16d8be27
dede53 <hallomarcel.hm@gmail.com>
2021-03-14 20:04:36 +0100

    fix port

--- src/app.ts
+++ src/app.ts
@@ -64,6 +64,6 @@ export class App {
         const privateKey = fs.readFileSync(process.env.KEY_PATH as string);
         const certificate = fs.readFileSync(process.env.CERT_PATH as string);
         const credentials = { key: privateKey, cert: certificate };
-        https.createServer(credentials, this.app).listen(this.secPort || process.env.PORT || 3000, "127.0.0.1");
+        https.createServer(credentials, this.app).listen(this.port || process.env.PORT || 3000, "127.0.0.1");
     }
 }
