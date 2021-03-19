9cd500d678fb65f05b7fefb1f5ae1d9b7eb47cbc
dede53 <hallomarcel.hm@gmail.com>
2021-03-19 23:58:16 +0100

    restructure topics

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { RowDataPacket } from "mysql2";
 import { UpdateUser, User } from "../interface/user";
 import pool from "../lib/db";
 import { removeEmptyStrings } from "../lib/helper";
+import { updateUserTopics } from "./user.topic.controller";
 
 
 
@@ -31,6 +32,9 @@ export async function getUserWithPassword(id: number): Promise<RowDataPacket> {
         "RIGHT JOIN topic ON (u.topic = topic.id AND u.user = ?) ";
     const [topics] = await pool.query<RowDataPacket[]>(query, [id]);
     row[0].topics = topics;
+    const lang = "select l.name, l.code, case when l.code = u.language then true else false end as selected from language l left join user_languages u on (l.code = u.language AND u.user = ?);";
+    const [languages] = await pool.query<RowDataPacket[]>(lang, [id]);
+    row[0].languages = languages;
     return row[0];
 }
 
@@ -60,6 +64,9 @@ export async function getUserByEmailWithPassword(email: string): Promise<RowData
         "RIGHT JOIN topic ON (u.topic = topic.id AND u.user = ?)";
     const [topics] = await pool.query<RowDataPacket[]>(query, [row[0].id]);
     row[0].topics = topics;
+    const lang = "select l.name, l.code, case when l.code = u.language then true else false end as selected from language l left join user_languages u on (l.code = u.language AND u.user = ?);";
+    const [languages] = await pool.query<RowDataPacket[]>(lang, [row[0].id]);
+    row[0].languages = languages;
     return row[0];
 }
 
@@ -75,35 +82,8 @@ export async function updateUserById(id: number, user: UpdateUser): Promise<void
     if(user.topics !== undefined){
         updateUserTopics(id, user.topics);
     }
+    if(user.languages !== undefined){
+        updateUserTopics(id, user.languages);
+    }
     await pool.query('UPDATE user SET username = ? WHERE id = ?', [user.username, id]);
-}
-
-export async function updateUserTopics(user: number, topics: [number]):Promise<void> {
-    await pool.query("DELETE FROM user_topics WHERE user = ?", [user]);
-    for (const i in topics) {
-        await pool.query("INSERT INTO user_topics (user, topic) VALUES (?, ?);", [user, topics[i] ]);
-    }
-}
-
-export async function updateUserTopic(user: number, topic: number):Promise<void> {
-    updateUserTopics(user, [topic]);
-}
-
-export async function addUserTopics(user: number, topics: number[]):Promise<void> {
-    for (const i in topics) {
-        await pool.query("INSERT INTO user_topics (user, topic) VALUES (?, ?);", [user, topics[i] ]);
-    }
-}
-
-export async function addUserTopic(user: number, topic: number):Promise<void> {
-    addUserTopics(user, [topic]);
-}
-
-export async function removeUserTopics(user: number, topics: [number]):Promise<void> {
-    await pool.query("DELETE FROM user_topics WHERE user = ? AND topic IN ?;", [user, topics]);
-
-}
-
-export async function removeUserTopic(user: number, topic: number):Promise<void> {
-    removeUserTopics(user, [topic]);
 }
---
+++ src/controllers/user.topic.controller.ts
@@ -0,0 +1,31 @@
+import pool from "../lib/db";
+
+
+export async function updateUserTopics(user: number, topics: [number]):Promise<void> {
+    await pool.query("DELETE FROM user_topics WHERE user = ?", [user]);
+    for (const i in topics) {
+        await pool.query("INSERT INTO user_topics (user, topic) VALUES (?, ?);", [user, topics[i] ]);
+    }
+}
+
+export async function updateUserTopic(user: number, topic: number):Promise<void> {
+    updateUserTopics(user, [topic]);
+}
+
+export async function addUserTopics(user: number, topics: number[]):Promise<void> {
+    for (const i in topics) {
+        await pool.query("INSERT INTO user_topics (user, topic) VALUES (?, ?);", [user, topics[i] ]);
+    }
+}
+
+export async function addUserTopic(user: number, topic: number):Promise<void> {
+    addUserTopics(user, [topic]);
+}
+
+export async function removeUserTopics(user: number, topics: [number]):Promise<void> {
+    await pool.query("DELETE FROM user_topics WHERE user = ? AND topic IN ?;", [user, topics]);
+}
+
+export async function removeUserTopic(user: number, topic: number):Promise<void> {
+    removeUserTopics(user, [topic]);
+}
