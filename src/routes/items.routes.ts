347f6cbbc5dfe8eb0dc3e32090c132c82012c42e
dede53 <hallomarcel.hm@gmail.com>
2021-03-21 21:38:41 +0100

    improve main route

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -5,18 +5,6 @@ import { authenticate, hasValidToken } from '../middleware';
 
 const router = Router();
 
-router.route('/')
-    .get(async (req: Request, res: Response) => {
-        req.token = hasValidToken(req.cookies.jwt);
-        if (req.cookies.jwt && req.token != false) {
-            const data = await getItemsWithUserData(req.token.id, req.headers["accept-language"]);
-            return res.status(200).json(data)
-        } else {
-            const data = await getAllItems(req.headers["accept-language"])
-            return res.status(200).json(data);
-        }
-    })
-
 router.route('/reviewed')
     .get(authenticate, async (req: Request, res: Response) => {
         return res.json(
@@ -62,12 +50,24 @@ router.route('/watchlist')
 router.route('/:limit/:startId?')
     .get(async (req: Request, res: Response) => {
         req.token = hasValidToken(req.cookies.jwt);
+        if(typeof req.query.topics === "string" && req.query.topics.length > 0){
+            req.query.topics = req.query.topics.split(",");
+        }
         if (req.cookies.jwt && req.token != false) {
-            return res.status(200).json(
-                await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
-            )
+            if (req.query.topics && req.query.topics.length && req.query.topics.length > 0) {
+                console.log("auth\ttopics\t => getItemsWithUserData");
+                return res.status(200).json(
+                    await getItemsWithUserData(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.query.topics as string[], req.headers["accept-language"])
+                );
+            } else {
+                console.log("auth\t\t => getSuggested");
+                return res.status(200).json(
+                    await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
+                );
+            }
         } else {
-            const data = await getItems(parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
+            console.log("\ttopics\t => getItems\t" + req.query.topics);
+            const data = await getItems(parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"], req.query.topics as string[])
             return res.status(200).json(data);
         }
     });
