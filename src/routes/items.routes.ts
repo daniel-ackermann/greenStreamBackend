3b5f733763a09476729a8e3345be48e6fe8b48ad
dede53 <hallomarcel.hm@gmail.com>
2021-03-28 15:08:55 +0200

    fix: return items according to topics

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -82,10 +82,24 @@ router.route('/feedbacks/:limit/:startId?')
 router.route('/search/:limit/:startId/:query?')
     .get(async (req: Request, res: Response) => {
         req.token = hasValidToken(req.cookies.jwt);
+        if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
+            req.query.topics = req.query.topics.split(",");
+        }
         let result;
         if (req.cookies.jwt && req.token != false) {
-            if(req.params.query === undefined){
-                result = await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"]);
+            console.log(req.params.query);
+            if(req.params.query === undefined ){
+                if (req.query.topics && req.query.topics.length && req.query.topics.length > 0) {
+                    console.log("auth\ttopics\t => getItemsWithUserData");
+                    return res.status(200).json(
+                        await getItemsWithUserData(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.query.topics as string[], req.headers["accept-language"])
+                    );
+                } else {
+                    console.log("auth\t\t => getSuggested");
+                    return res.status(200).json(
+                        await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
+                    );
+                }
             }else{
                 result = await getSearchResultUser(req.params.query, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[],  parseInt(req.token.id), req.headers["accept-language"])
             }
