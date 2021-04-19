4477f12a9767cb4c2828dc22a37ccdc8cbd005f6
dede53 <hallomarcel.hm@gmail.com>
2021-04-19 18:20:36 +0200

    fix: catch userinput error

--- src/routes/items.routes.ts
+++ src/routes/items.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { Request, Response } from '../interface/custom.request'
 import { getItems, getReviewedItemsByUser, getItemsByUser, getItemsToReview, getLikedItems, getWatchListItems, getWatchedItems, getItemsWithUserData, getSuggestedItems, getItemsWithFeedback, getSearchResult, getSearchResultUser } from '../controllers/items.controller'
 import { authenticate, hasValidToken } from '../middleware';
+import { RowDataPacket } from 'mysql2';
 
 const router = Router();
 
@@ -10,9 +11,13 @@ router.route('/reviewed/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        return res.json(
-            await getReviewedItemsByUser(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-        );
+        try {
+            return res.json(
+                await getReviewedItemsByUser(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+            );
+        } catch (e) {
+            return res.status(422).send();
+        }
     });
 
 router.route('/review/:limit/:startId?')
@@ -20,9 +25,13 @@ router.route('/review/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        res.json(
-            await getItemsToReview(req.token.id, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-        );
+        try {
+            return res.json(
+                await getItemsToReview(req.token.id, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+            );
+        } catch (e) {
+            return res.status(422).send();
+        }
     })
 
 router.route('/created/:limit/:startId?')
@@ -30,9 +39,13 @@ router.route('/created/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        return res.json(
-            await getItemsByUser(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-        )
+        try {
+            return res.json(
+                await getItemsByUser(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+            )
+        } catch (e) {
+            return res.status(422).send();
+        }
     })
 
 router.route('/liked/:limit/:startId?')
@@ -40,9 +53,13 @@ router.route('/liked/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        return res.json(
-            await getLikedItems(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-        )
+        try {
+            return res.json(
+                await getLikedItems(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+            )
+        } catch (e) {
+            return res.status(422).send();
+        }
     })
 
 router.route('/watched/:limit/:startId?')
@@ -50,9 +67,13 @@ router.route('/watched/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        return res.json(
-            await getWatchedItems(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-        )
+        try {
+            return res.json(
+                await getWatchedItems(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+            )
+        } catch (e) {
+            return res.status(422).send();
+        }
     })
 
 router.route('/watchlist/:limit/:startId?')
@@ -60,22 +81,30 @@ router.route('/watchlist/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        return res.json(
-            await getWatchListItems(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-        )
+        try {
+            return res.json(
+                await getWatchListItems(parseInt(req.token.id, 10), parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+            )
+        } catch (e) {
+            return res.status(422).send();
+        }
     })
 
 router.route('/feedbacks/:limit/:startId?')
     .get(authenticate, async (req: Request, res: Response) => {
         req.token = hasValidToken(req.cookies.jwt);
-        if (req.cookies.jwt && req.token != false) {
-            return res.json(
-                await getItemsWithFeedback(parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[], parseInt(req.token.id))
-            );
-        } else {
-            return res.json(
-                await getItemsWithFeedback(parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
-            );
+        try {
+            if (req.cookies.jwt && req.token != false) {
+                return res.json(
+                    await getItemsWithFeedback(parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[], parseInt(req.token.id))
+                );
+            } else {
+                return res.json(
+                    await getItemsWithFeedback(parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[])
+                );
+            }
+        } catch (e) {
+            return res.status(422).send();
         }
     });
 
@@ -85,11 +114,10 @@ router.route('/search/:limit/:startId/:query?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        let result;
+        let result: RowDataPacket[];
         if (req.cookies.jwt && req.token != false) {
-            console.log(req.params.query);
-            try{
-                if(req.params.query === undefined ){
+            try {
+                if (req.params.query === undefined) {
                     if (req.query.topics && req.query.topics.length && req.query.topics.length > 0) {
                         console.log("auth\ttopics\t => getItemsWithUserData");
                         return res.status(200).json(
@@ -101,17 +129,21 @@ router.route('/search/:limit/:startId/:query?')
                             await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
                         );
                     }
-                }else{
-                    result = await getSearchResultUser(req.params.query, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[],  parseInt(req.token.id), req.headers["accept-language"])
+                } else {
+                    result = await getSearchResultUser(req.params.query, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[], parseInt(req.token.id), req.headers["accept-language"])
                 }
-            }catch(e){
-                return res.status(200).json([]);
+            } catch (e) {
+                return res.status(422).json();
             }
         } else {
-            if(req.params.query === undefined){
-                result = await getItems(parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"], req.query.topics as string[]);
-            }else{
-                result = await getSearchResult(req.params.query, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[], req.headers["accept-language"])
+            try {
+                if (req.params.query === undefined) {
+                    result = await getItems(parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"], req.query.topics as string[]);
+                } else {
+                    result = await getSearchResult(req.params.query, parseInt(req.params.limit), parseInt(req.params.startId), req.query.topics as string[], req.headers["accept-language"])
+                }
+            } catch (e) {
+                return res.status(422).send();
             }
         }
         return res.json(result);
@@ -123,22 +155,26 @@ router.route('/:limit/:startId?')
         if (typeof req.query.topics === "string" && req.query.topics.length > 0) {
             req.query.topics = req.query.topics.split(",");
         }
-        if (req.cookies.jwt && req.token != false) {
-            if (req.query.topics && req.query.topics.length && req.query.topics.length > 0) {
-                console.log("auth\ttopics\t => getItemsWithUserData");
-                return res.status(200).json(
-                    await getItemsWithUserData(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.query.topics as string[], req.headers["accept-language"])
-                );
+        try {
+            if (req.cookies.jwt && req.token != false) {
+                if (req.query.topics && req.query.topics.length && req.query.topics.length > 0) {
+                    console.log("auth\ttopics\t => getItemsWithUserData");
+                    return res.status(200).json(
+                        await getItemsWithUserData(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.query.topics as string[], req.headers["accept-language"])
+                    );
+                } else {
+                    console.log("auth\t\t => getSuggested");
+                    return res.status(200).json(
+                        await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
+                    );
+                }
             } else {
-                console.log("auth\t\t => getSuggested");
-                return res.status(200).json(
-                    await getSuggestedItems(parseInt(req.token.id, 10), parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"])
-                );
+                console.log("\ttopics\t => getItems\t" + req.query.topics);
+                const data = await getItems(parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"], req.query.topics as string[])
+                return res.status(200).json(data);
             }
-        } else {
-            console.log("\ttopics\t => getItems\t" + req.query.topics);
-            const data = await getItems(parseInt(req.params.startId) || 0, parseInt(req.params.limit), req.headers["accept-language"], req.query.topics as string[])
-            return res.status(200).json(data);
+        } catch (e) {
+            return res.status(422).send();
         }
     });
 
